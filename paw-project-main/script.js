a6797fa78c6819130909a5a7c913ccbddabcfe04
lina <linaboumghar0@gmail.com>
2023-10-30 23:38:22 +0100

    ading new pages

--- paw-project-main/script.js
+++ paw-project-main/script.js
@@ -1,5 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
 const logregBox=document.querySelector(".logreg-box");
 const loginLink=document.querySelector(".login-link");
 const registerLink=document.querySelector(".register-link");
-registerLink.addEventListener("click",()=>{logregBox.classList.add("active");})
-loginLinkLink.addEventListener("click",()=>{logregBox.classList.remove("active");})
+registerLink.addEventListener("click", () =>
+{logregBox.classList.add("active");
+});
+loginLink.addEventListener("click", () =>
+{logregBox.classList.remove("active");
+});
+});
